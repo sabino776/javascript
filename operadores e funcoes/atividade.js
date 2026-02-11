98d8c21dee19076d3ece136c36efa3e5b6c82de9
sabino776 <vitor.sabino@aluno.senai.br>
2026-02-11 15:18:02 -0300

    dia11

---
+++ operadores e funcoes/atividade.js
@@ -0,0 +1,34 @@
+function mostrarHoraAtual() {
+    let agora = new Date();
+    let horas = agora.getHours();
+    let minutos = agora.getMinutes();
+    let segundos = agora.getSeconds();
+
+    console.log(`${horas}:${minutos}:${segundos}`);
+}
+
+mostrarHoraAtual();
+function calcularNumeros(num1, num2) {
+    let soma = num1 + num2;
+    let media = soma / 2;
+
+    console.log("Soma:", soma);
+    console.log("Média:", media);
+}
+
+calcularNumeros(2, 2);
+function analisarNome(nome) {
+    console.log("Quantidade de letras:", nome.length);
+    console.log("Nome em maiúsculo:", nome.toUpperCase());
+}
+
+analisarNome("Vitor");
+function verificarHTML(frase) {
+    if (frase.includes("HTML")) {
+        console.log("A frase contém a palavra HTML");
+    } else {
+        console.log("A frase NÃO contém a palavra HTML");
+    }
+}
+
+verificarHTML("Estou estudando HTML e CSS");
